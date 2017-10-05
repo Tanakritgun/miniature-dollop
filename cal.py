b39fb1f0c2db13cc38bff18d972b7c65401a8c5b
nutorbitx <nutorbitx@gmail.com>
2017-10-05 11:05:57 +0700

    add summation

--- cal.py
+++ cal.py
@@ -1,11 +1,14 @@
 subtraction = lambda num1, num2: num1-num2
+summation = lambda num1, num2: num1+num2
 
 def cal(num1,num2):
 	summ = summation(num1, num2)
 	subb = subtraction(num1, num2)
 	return subb, summ
 
+'''
 def summation(n1, n2):
 	return n1+ n2
+'''
 
 cal(int(input()), int(input()))
